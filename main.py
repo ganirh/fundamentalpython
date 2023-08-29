6813f00945e21f414e6a3ecbf2b7274695ff59b9
Hasan Gani Rachmatullah <ganirh0612@gmail.com>
2023-08-30 05:22:32 +0700

    Ini Commit Kedua

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # Fundamental Python
+
+import requests
+
+url = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+res = requests.get(url)
+
+print("Status Code Situs: {}".format(res.status_code))
